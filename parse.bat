1e8a64d97570d03910743b4c14e64daa841cdad7
LMW <williamchange@gmail.com>
2016-06-06 12:25:01 +0800

    script update
    - simplify script a little bit
    - now cleanup links before processing them from Links.txt
    - now move all downloaded photos to export correctly
    - reduce the creation of unnecessary temporary files

--- parse.bat
+++ parse.bat
@@ -1,29 +1,28 @@
 @echo off
 ::settings
 setlocal EnableDelayedExpansion
-
-::cleaning up links
+mkdir export
+::cleaning up links #1
 for /F "tokens=*" %%A in (Links.txt) do (
+   echo %%A | bin\grep -ao ".*\/.*[0-9]\/">>links.tmp
+)
+::cleaning up links #2
+for /F "tokens=*" %%B in (links.tmp) do (
    SET /A vidz=!vidz! + 1
    title processing link !vidz! 
-   bin\wget -qO "!vidz!a.txt" "https://500px.com/oembed?url=%%A&format=json"
-   bin\grep -ao "https:\/\/d.[a-z0-9].*,.w" "!vidz!a.txt">!vidz!b.txt
-   bin\sed -f cleanup.sed !vidz!b.txt>>parselinks.txt
-   rm !vidz!*
+   bin\wget -qO "!vidz!a.txt" "https://500px.com/oembed?url=%%B&format=json"
+   bin\grep -ao "https:\/\/d.[a-z0-9].*,.w" "!vidz!a.txt" | bin\sed -f cleanup.sed>>parselinks.txt
+   bin\rm !vidz!*
 )
-::bin\rm 1 a b
 ::download data from links
-title downloading photos
-for /F "tokens=*" %%i in (parselinks.txt) do (
-	cd export
-    wget -q "%%i"
-	cd ..
+for /F "tokens=*" %%c in (parselinks.txt) do (
+    title downloading photo %%c
+	wget -q "%%c"
 )
-cd export
 FOR /F %%i in ('dir /b/s/A-d') DO (
   if "%%~xi" == "" rename "%%~fi" "%%~ni.jpg"
 )
-cd ..
-rm parselinks.*
+bin\cp *.jpg export\
+bin\rm parselinks.* links.tmp *.jpg
 title done
 ::copy NUL Links.txt
