e440c66cd5282ce188aabdbe63c6208133ea2a4d
LMW <williamchange@gmail.com>
2016-06-13 22:13:41 +0800

    add showing progress of total images to be processed

--- parse.bat
+++ parse.bat
@@ -2,6 +2,9 @@
 setlocal EnableDelayedExpansion
 IF NOT EXIST export mkdir export
 
+bin\wc -l links.txt | bin\grep -ao "[0-9]*">no
+set /p no=<no
+title Total images: %no%
 
 for /F "tokens=*" %%A in (Links.txt) do (
    echo %%A | bin\grep -ao ".*\/.*[0-9]\/">>links.tmp
@@ -10,7 +13,7 @@ for /F "tokens=*" %%A in (Links.txt) do (
 
 for /F "tokens=*" %%B in (links.tmp) do (
    SET /A vidz=!vidz! + 1
-   echo processing link !vidz! 
+   echo processing link !vidz!/%no% 
    bin\wget -qO "!vidz!a.txt" "https://500px.com/oembed?url=%%B&format=json"
    bin\grep -ao "https:\/\/d.[a-z0-9].*,.w" "!vidz!a.txt" | bin\sed -f cleanup.sed>>parselinks.txt
    bin\rm !vidz!*
@@ -19,7 +22,7 @@ for /F "tokens=*" %%B in (links.tmp) do (
 
 
 for /F "tokens=*" %%c in (parselinks.txt) do (
-    echo downloading photo %%c
+    echo downloading photo %%c/%no% 
 	wget -q "%%c"
 	cls
 )
@@ -29,6 +32,6 @@ FOR /F %%i in ('dir /b/s/A-d') DO (
   if "%%~xi" == "" rename "%%~fi" "%%~ni.jpg"
 )
 bin\cp *.jpg export\
-bin\rm parselinks.* links.tmp *.jpg
+bin\rm parselinks.* links.tmp *.jpg *.1
 echo done.
 copy NUL Links.txt
